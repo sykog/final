f57909f20fa98ce671634355d7a6af3f8724a49b
Sam Ourn <sourn@mail.greenriver.edu>
2018-02-08 13:58:47 -0500

    Created index.php and started profile page

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+//Require the autoload file
+require_once ('vendor/autoload.php');
+
+//Turn on error reporting
+ini_set('display-errors', 1);
+error_reporting(E_ALL);
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Define a route using parameters
+$f3->route('GET /@name', function($f3, $params) {
+
+    $f3->set('username', $params['username']);
+    $template = new Template();
+    echo $template->render('pages/profile.html');
+}
+);
+
+//Run Fat-Free
+$f3->run();

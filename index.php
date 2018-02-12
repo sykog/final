9ebb6e23536354ddfc64a4cc3eff48caadad8689
Sam Ourn <sourn@mail.greenriver.edu>
2018-02-12 13:03:43 -0500

    Added styling to profile.php, created navbar

--- index.php
+++ index.php
@@ -10,7 +10,15 @@ error_reporting(E_ALL);
 //Create an instance of the Base class
 $f3 = Base::instance();
 
-//Define a route using parameters
+//Define a route to go to home page
+$f3->route('GET /', function() {
+
+    $template = new Template();
+    echo $template->render('pages/home.php');
+}
+);
+
+//Define a route using parameters to get to a user's profile
 $f3->route('GET /@username', function($f3, $params) {
 
     $f3->set('username', $params['username']);
---
+++ pages/home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+</head>
+<body>
+    <h1>This is the home page.</h1>
+</body>
+</html>

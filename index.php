539f3053eeb0aad42f1f1a2c7500e8d177e34a5f
Antonio Suarez <asuarez2@mail.greenriver.edu>
2018-03-01 21:35:51 -0500

    fixed navbar links

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 //Require the autoload file
 require_once ('vendor/autoload.php');
+//Start the session
+session_start();
 
 //Turn on error reporting
 ini_set('display-errors', 1);
@@ -9,9 +11,14 @@ error_reporting(E_ALL);
 
 //Create an instance of the Base class
 $f3 = Base::instance();
+// set debug level
+$f3->set('DEBUG', 3);
+
+$_SESSION['user'] = "sykog";
+$f3->set('user', $_SESSION['user']);
 
 //Define a route to go to home page
-$f3->route('GET /', function() {
+$f3->route('GET /', function($f3, $params) {
 
     $template = new Template();
     echo $template->render('pages/navbar.html');
@@ -19,7 +26,7 @@ $f3->route('GET /', function() {
 });
 
 //Define a route using parameters to get to a user's profile
-$f3->route('GET /@username', function($f3, $params) {
+$f3->route('GET /profile/@user', function($f3, $params) {
 
     $f3->set('username', $params['username']);
     $template = new Template();
